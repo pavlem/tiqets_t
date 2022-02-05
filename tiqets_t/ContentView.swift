52d95b02e8b620b484d96921bc2818dbcc711e2f
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2022-02-05 13:03:04 +0100

    Initial commit

---
+++ tiqets_t/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tiqets_t
+//
+//  Created by Pavle Mijatovic on 5.2.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tiqets_t/tiqets_tApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiqets_tApp.swift
+//  tiqets_t
+//
+//  Created by Pavle Mijatovic on 5.2.22..
+//
+
+import SwiftUI
+
+@main
+struct tiqets_tApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
